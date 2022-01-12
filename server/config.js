4cdd11356085bd2a57da3a24125d1dce470c838b
Volpito <mpj.rigot@gmail.com>
2022-01-12 17:02:35 +0100

    Mailer server ok

--- server/config.js
+++ server/config.js
@@ -6,6 +6,7 @@ const nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
+    Allow: '/send',
     user: process.env.email, // your email address to send email from
     pass: process.env.password // your gmail account password
   }
